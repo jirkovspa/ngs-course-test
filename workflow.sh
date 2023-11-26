d69c2d07eab71f819d483bd9a037dc2fe359a5f3
Pavel Jirkovsky <jirkovspa@natur.cuni.cz>
2023-11-26 10:54:12 +0100

    correstion in workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -7,4 +7,4 @@ INPUT=data/Ensembl.NCBIM37.67.bed
 < $INPUT cut -f1 | sort | uniq -c | awk "{print $2, $1}" | head
 
 #Shell filtering function
- fastq-min-length() { paste - - - - | sort | awk /v min_len=$1 '(length($2) > min_len)' | tr "\t" "\n" ;}
+ fastq-min-length() { paste - - - - | sort | awk /v min_len="$1" '(length($2) > min_len)' | tr "\t" "\n" ;}
