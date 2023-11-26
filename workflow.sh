f83f10951e8351426078e04f16a408ed0b362da2
Pavel Jirkovsky <jirkovspa@natur.cuni.cz>
2023-11-26 10:49:36 +0100

    adding workflow.sh

---
+++ workflow.sh
@@ -0,0 +1,10 @@
+
+# Data exploration: bumber of variants
+INPUT=data/Ensembl.NCBIM37.67.bed
+< $INPUT cut -f1 | sort | uniq -c | less
+
+#Table of the above
+< $INPUT cut -f1 | sort | uniq -c | awk "{print $2, $1}" | head
+
+#Shell filtering function
+ fastq-min-length() { paste - - - - | sort | awk /v min_len=$1 '(length($2) > min_len)' | tr "\t" "\n" ;}
